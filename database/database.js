2ad9ac94d2438712ba6e135d155ae5df9e5c64d7
nitin2353 <154704828+nitin2353@users.noreply.github.com>
2025-06-23 16:35:24 +0530

    Add files via upload

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connectDB() {
     try {
-        await mongoose.connect(process.env.MONGO_URI); // no options needed in Mongoose 8+
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('MongoDB connected with Mongoose');
     } catch (err) {
         console.error('MongoDB Not connected', err);

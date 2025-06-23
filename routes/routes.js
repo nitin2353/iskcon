07306dff76a78758382767fd6a594acb130db9f1
nitin2353 <154704828+nitin2353@users.noreply.github.com>
2025-06-23 16:31:29 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -5,130 +5,142 @@ const path = require('path');
 const session = require('express-session');
 const User = require('../module/user');
 
+// Session Setup
 router.use(session({
-    secret: process.env.secret,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: false,
     cookie: {
         maxAge: 1000 * 60 * 60 * 24 * 365
     } 
-}))
+}));
 
+// Routes
+router.get('/account', (req, res) => {
+    res.render('signup');
+});
 
 router.get('/', (req, res) => {
-    res.render('signup');
-})
-
-router.get('/haribol', (req, res) => {
-    if(req.session.isAuth == true){
+    if (req.session.isAuth === true) {
         res.render('index');
+    } else {
+        res.redirect('/account');
     }
-    else{
-        res.redirect('/')
-    }
-})
+});
 
 router.get('/users', (req, res) => {
     res.render('users');
-})
+});
 
-
-router.get('/allUsers', async(req, res) => {
-    const users = await User.find().sort({ mala_count : -1});
-    res.json(users);
-})
-
-router.get('/myDetail', async(req, res) => {
-    const users = await User.find({ phone : req.session.mobile});
-    res.json(users);
-})
-
-
-
-// --------------------signUp--------------------
-var img_path;
-const storage = multer.diskStorage({
-    destination :(req, file, cb) => {
-        return cb(null, path.join(__dirname,'../users_img'))
-    },
-    filename : (req, file, cb) => {
-        img_path = `${Date.now()}-${file.originalname}`;
-        return cb(null,`${Date.now()}-${file.originalname}`);
+router.get('/allUsers', async (req, res) => {
+    try {
+        const users = await User.find().sort({ mala_count: -1 });
+        res.json(users);
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
     }
-})
+});
 
-const upload = multer({storage : storage})
+router.get('/myDetail', async (req, res) => {
+    try {
+        const users = await User.find({ phone: req.session.mobile });
+        res.json(users);
+    } catch (error) {
+        res.status(500).send('Error: ' + error.message);
+    }
+});
 
+// -------------------- Sign Up --------------------
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../users_img'));
+    },
+    filename: (req, file, cb) => {
+        const filename = `${Date.now()}-${file.originalname}`;
+        cb(null, filename);
+    }
+});
+
+const upload = multer({ storage: storage });
 
 router.post('/signupYouthData', upload.single('photo'), async (req, res) => {
-    
-    const newUser = new User({
-        name: req.body.name,
-        image: img_path,
-        phone:  req.body.phone,
-        password: req.body.password,
-        date: Date.now(),
-        count: 0,
-        mala_count: 0
-    }) 
     try {
+
+
+        const newUser = new User({
+            name: req.body.name,
+            image: req.file.filename, 
+            phone: req.body.phone,
+            password: req.body.password,
+            date: Date.now(),
+            count: 0,
+            mala_count: 0,
+            total_mala: 0
+        });
+
         await newUser.save();
         req.session.isAuth = true;
-        req.session.mobile=phone;
-        res.redirect('/haribol');
+        req.session.mobile = req.body.phone;
+        res.redirect('/');
     } catch (error) {
-        res.send('not submitted' + error);
+        res.status(500).send('Error: ' + error.message);
     }
-})
+});
 
-
-// ---------------------------------login---------------------------------
-
-
-router.post('/loginYouthData', async(req, res) => {
-
-    phone = req.body.phone;
-    password = req.body.password;    
+// -------------------- Login --------------------
+router.post('/loginYouthData', async (req, res) => {
+    const phone = req.body.phone;
+    const password = req.body.password;
 
     try {
-        let data =  await User.findOne({$and : [{phone : {$eq : phone}}, {password : {$eq : password}}]});
-        if(data){
-          req.session.isAuth=true;
-          req.session.mobile=phone;
-          res.redirect('/haribol');
-        }else{
-            res.redirect('/')
+        const data = await User.findOne({ phone: phone });
+
+        if (data) {
+
+            const isMatch = (data.password === password); 
+
+            if (isMatch) {
+                req.session.isAuth = true;
+                req.session.mobile = phone;
+                res.redirect('/');
+            } else {
+                res.redirect('/account');
+            }
+        } else {
+            res.redirect('/account');
         }
     } catch (error) {
-        res.redirect('/')
+        res.status(500).send('Error: ' + error.message);
     }
-})
-// -------------------------Mala Update------------------------
+});
 
-
-var beats = 0;
+// -------------------- Mala Update --------------------
 router.post('/chantingupdate', async (req, res) => {
-    //console.log(req.body.counting);
-    try{
-        let chant = await User.findOne({phone : req.session.mobile});
-        if(chant.count == 108){
-            beats=0;
-            mala = chant.mala_count + 1;
-            await User.updateOne({phone : req.session.mobile}, {$set : {"count" : beats}})
-            await User.updateOne({phone : req.session.mobile}, {$set : {"mala_count" : mala}})
-        }
-        else{
-            beats = 0;
-            beats = 1 + chant.count;
-            await User.updateOne({phone : req.session.mobile}, {$set : {"count" : beats}})
-        }
-        res.send(chant)
+    try {
+        const chant = await User.findOne({ phone: req.session.mobile });
 
-    }catch(error){
-        console.log('Error Occured' + error);
+        if (!chant) {
+            return res.status(404).send('User not found');
+        }
+
+        if (chant.count === 108) {
+            await User.updateOne(
+                { phone: req.session.mobile },
+                { $set: { count: 0 }, $inc: { mala_count: 1 } }
+            );
+        } else {
+            await User.updateOne(
+                { phone: req.session.mobile },
+                { $inc: { count: 1 } }
+            );
+        }
+
+        const updatedUser = await User.findOne({ phone: req.session.mobile });
+        res.json(updatedUser);
+    } catch (error) {
+        console.log('Error Occurred: ' + error);
+        res.status(500).send('Error: ' + error.message);
     }
-})
-
-
+});
 
 module.exports = router;
